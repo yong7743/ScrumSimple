b04e2f0783e8d61d718d863b14b57e92c7de6fa8
ben7276 <btan@esri.com>
2017-10-31 09:26:10 +0800

    remove not used libraries

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,20 +4,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
     SubmitField, DateField
-from wtforms.validators import Required, Length, Email, Regexp
-from wtforms import ValidationError
-from flask_pagedown.fields import PageDownField
-from app.models import Report, User
+from wtforms.validators import Required
 
 
 class ReportForm(FlaskForm):
     date = DateField('Date?', validators=[Required()])
     body = TextAreaField("Today?", validators=[Required()])
-    submit = SubmitField('Submit')
-
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
 
--- app/models.py
+++ app/models.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-import hashlib
-from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from markdown import markdown
-import bleach
 from flask import current_app, request, url_for
-from flask_login import UserMixin, AnonymousUserMixin
+from flask_login import UserMixin
 from . import db, login_manager
 
 
