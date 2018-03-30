e8b7ce4a61b45de47ea8d108e51d86a33d349901
ben7276 <btan@esri.com>
2018-03-30 17:32:20 +0800

    add more flash msg

--- app/auth/views.py
+++ app/auth/views.py
@@ -39,13 +39,16 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
+        if user is None:
+            flash('Can not find the user, please register.')
+        elif user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
             return redirect(next)
-        flash('Invalid username or password.')
+        else:
+            flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
 
