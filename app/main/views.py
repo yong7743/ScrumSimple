c02dd379962f8386e089c3b7f1f9f88aa7f63659
ben7276 <btan@esri.com>
2018-05-07 14:00:51 +0800

    test

--- app/main/views.py
+++ app/main/views.py
@@ -41,7 +41,7 @@ def get_duty_text():
         main_dir = os.path.abspath(os.path.dirname(__file__))
         member_path = os.path.join(main_dir, 'extension/member.json')
         dutySchedule = DutySchedule(member_path, datetime.date(2018, 5, 6))
-        duty_name = dutySchedule.get_member_onduty(today_date)
+        #duty_name = dutySchedule.get_member_onduty(today_date)
         members_name = dutySchedule.get_members()
         duty_name = "Jasmine"
         text = duty_name + "! o(*￣︶￣*)o ~~~~ Orders:" + ', '.join(members_name)
