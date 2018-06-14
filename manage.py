7698ce4be42f920045c8dbea9da0436d60633957
Ben Tan <ben7276@esri.com>
2018-06-14 03:13:30 -0700

    add support for tornado

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ if os.path.exists('.env'):
 
 from app import create_app, db
 from app.models import User, Report
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -30,6 +30,8 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Report=Report)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", Server(host="0.0.0.0", port=5001))
+
 
 
 @manager.command
